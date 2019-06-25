5766ea031780cb89fdb77444a4e5f56921f8f2e4
Martin Vega <mvegaca@outlook.com>
2019-06-25 14:48:52 +0200

    Add doc links

--- TreeViewPoC/TreeViewPoC/Views/TreeViewPage.xaml.cs
+++ TreeViewPoC/TreeViewPoC/Views/TreeViewPage.xaml.cs
@@ -6,6 +6,9 @@ using Windows.UI.Xaml.Navigation;
 
 namespace TreeViewPoC.Views
 {
+    // For more info about the TreeView Control see
+    // https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/tree-view
+    // For other samples, get the XAML Controls Gallery app http://aka.ms/XamlControlsGallery
     public sealed partial class TreeViewPage : Page
     {
         public TreeViewViewModel ViewModel { get; } = new TreeViewViewModel();
@@ -19,6 +22,6 @@ namespace TreeViewPoC.Views
         {
             base.OnNavigatedTo(e);
             await ViewModel.LoadDataAsync();
-        }        
+        }
     }
 }
